cdb060a82fa9ace0c65abb26aa61c6c0f35b2a69
Caleb Evans <caevans@redhat.com>
2024-04-18 00:28:26 -0600

    Add entrypoint

---
+++ cmd/app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"hypha/api/internal/config"
+	"hypha/api/internal/db"
+	"hypha/api/internal/db/tables"
+	"hypha/api/internal/http"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "Path to configuration file")
+	flag.Parse()
+
+	cfg, err := config.ReadConfig(*configPath)
+	if err != nil {
+		log.Fatal("Could not read config: ", err)
+	}
+
+	dbConn, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatal("Could not connect to database: ", err)
+	}
+
+	if err := db.AutoMigrate(dbConn, &tables.Product{}, &tables.Test{}); err != nil {
+		log.Fatalf("Could not migrate tables: %v", err)
+	}
+
+	router := gin.Default()
+	dbConnWrapper := &db.DBConnWrapper{DB: dbConn}
+	http.SetupRoutes(router, dbConnWrapper)
+
+	router.Run("localhost:5052")
+}

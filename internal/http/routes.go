a68796f7086d0fcf4d68f006b7b56ba84ff72352
Caleb Evans <caevans@redhat.com>
2024-07-30 01:35:03 -0600

    Add new /db/integration route

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"hypha/api/internal/http/db/integration"
 	"hypha/api/internal/http/db/product"
 	"hypha/api/internal/http/db/utils"
 	"hypha/api/utils/logging"
@@ -14,4 +15,5 @@ func InitRoutes(router *gin.Engine, dbOperations utils.DatabaseOperations) {
 	log.Info().Msg("Initializing routes")
 	dbGroup := router.Group("/db")
 	product.InitProductRoutes(dbGroup, dbOperations)
+	integration.InitIntegrationRoutes(dbGroup, dbOperations)
 }
